a191bf9196cc04dffb7f3f5eb67a0d27b3cbd3c5
RYUKxAYU <93836508+RYUKxAYU@users.noreply.github.com>
2024-12-28 09:44:27 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,4 @@
+class Config:
+    SECRET_KEY = 'uvpqa238cjkdab9kajwvbu32891ibkwjvba38ckacwj'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:rootpassword@localhost/eco_friendly'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
